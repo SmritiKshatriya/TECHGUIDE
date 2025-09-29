710623ccf24abafa9ffffd998ff137e02b480f94
Neelam-2005 <neelamwaghchoure@gmail.com>
2025-09-29 13:37:45 +0530

    Add files via upload
    Mini Quiz Script

---
+++ N_mini_quiz.py
@@ -0,0 +1,8 @@
+language = input("Which programming language do you like the most? ")
+experience = input("How many years of coding experience do you have? ")
+field = input("Which field of technology interests you the most? ")
+
+print("\nHere are your answers:")
+print("Favorite Programming Language:", language)
+print("Coding Experience (in years):", experience)
+print("Technology Field of Interest:", field)
