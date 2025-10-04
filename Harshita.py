1b60861882c93376ae18dfa0c510f8e0ffa62379
Harshita-24105 <harshu24105@gmail.com>
2025-10-04 11:16:07 +0530

    A:python file

---
+++ Harshita.py
@@ -0,0 +1,13 @@
+import random
+
+quotes = [
+    "Believe in yourself and all that you are.",
+    "Do it now. Sometimes 'later' becomes 'never'.",
+    "Mistakes are proof that you are trying.",
+    "Push yourself, because no one else is going to do it for you.",
+    "Dream it. Wish it. Do it.",
+    "Great things never come from comfort zones.",
+]
+
+print("✨ Your Quote of the Day ✨")
+print(random.choice(quotes))
